0e19009e447554ba623f22593a3a0f5620debf34
Zhiyong Pan <zy.pan@wowtech-inc.com>
2014-09-17 22:37:13 +0800

    Add script: 复制 om_im_ui repo 中的一个图片资源到 Android 资源目录中。

---
+++ dev_tools/cp_drawable.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 复制 om_im_ui repo 中的一个图片资源到 Android 资源目录中。
+
+if [[ $# -lt 1 ]]; then
+    echo Usage: $0 src [dest]
+    echo -e '\tsrc,dest\tfilename without ext.'
+    exit
+fi
+
+from=$1
+if [ $# -lt 2 ]; then
+    to=$from
+else
+    to=$2
+fi
+
+for dpi in mdpi hdpi xhdpi
+do
+    echo cp -f ../om_im_ui/client/android/$dpi/$from.png project/res/drawable-$dpi/$to.png
+    cp -f ../om_im_ui/client/android/$dpi/$from.png project/res/drawable-$dpi/$to.png
+done
+

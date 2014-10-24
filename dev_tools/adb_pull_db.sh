5dfb75b8bc7cc35b40cfd36c6f1d372af2ac517b
Zhiyong Pan <zy.pan@wowtech-inc.com>
2014-10-24 22:36:42 +0800

    update dev tool script.

--- dev_tools/adb_pull_db.sh
+++ dev_tools/adb_pull_db.sh
@@ -1,1 +1,2 @@
-adb pull /data/data/co.onemeter.oneapp/databases/wowtalkdb && sqlite3 wowtalkdb
+db=wowtalkdb_8a0dc925-b22a-4d6b-8b9f-ecc7159dda14
+adb pull /data/data/co.onemeter.oneapp/databases/$db && sqlite3 $db

c39e86f0fbf84fa499e621a16a11efe0c26438a5
Zhiyong Pan <zy.pan@wowtech-inc.com>
2014-11-08 17:58:54 +0800

    add page to add class.

--- project/src/co/onemeter/oneapp/ui/AddClassActivity.java
+++ project/src/co/onemeter/oneapp/ui/AddClassActivity.java
@@ -1,36 +1,35 @@
 package co.onemeter.oneapp.ui;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import co.onemeter.oneapp.R;
 import com.androidquery.AQuery;
 
 /**
- * Created by pzy on 9/18/14.
+ * 添加课堂
+ * Created by pzy on 11/8/14.
  */
-public class HomeActivity extends Activity implements View.OnClickListener {
+public class AddClassActivity extends Activity implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_add_class);
 
         AQuery q = new AQuery(this);
-        q.find(R.id.btn_event).clicked(this);
-        q.find(R.id.vg_event).clicked(this);
+        q.find(R.id.title_back).clicked(this);
         q.find(R.id.btn_add).clicked(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn_event:
-            case R.id.vg_event:
-                startActivity(new Intent(this, EventActivity.class));
+            case R.id.title_back:
+                onBackPressed();
                 break;
             case R.id.btn_add:
-                startActivity(new Intent(this, AddClassActivity.class));
+                Toast.makeText(this, R.string.not_implemented, Toast.LENGTH_SHORT).show();
                 break;
         }
     }

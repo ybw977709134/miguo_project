7a3b54d9de15f98daddac239173329c4b8cf2e78
Zhiyong Pan <zy.pan@wowtech-inc.com>
2014-11-10 17:04:21 +0800

    add stub page: my classes, my homework.

--- project/src/co/onemeter/oneapp/ui/HomeActivity.java
+++ project/src/co/onemeter/oneapp/ui/HomeActivity.java
@@ -6,20 +6,40 @@ import android.os.Bundle;
 import android.view.View;
 import co.onemeter.oneapp.R;
 import com.androidquery.AQuery;
+import org.wowtalk.ui.MessageBox;
 
 /**
  * Created by pzy on 9/18/14.
  */
 public class HomeActivity extends Activity implements View.OnClickListener {
+    MessageBox msgbox;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        msgbox = new MessageBox(this);
+
         AQuery q = new AQuery(this);
         q.find(R.id.btn_event).clicked(this);
         q.find(R.id.vg_event).clicked(this);
+        q.find(R.id.btn_study).clicked(this);
+        q.find(R.id.vg_study).clicked(this);
+        q.find(R.id.btn_bonus).clicked(this);
+        q.find(R.id.vg_bonus).clicked(this);
+        q.find(R.id.btn_grow).clicked(this);
+        q.find(R.id.vg_grow).clicked(this);
         q.find(R.id.btn_add).clicked(this);
+        q.find(R.id.btn_homework).clicked(this);
+        q.find(R.id.btn_myclasses).clicked(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        msgbox.dismissWait();
+        msgbox.dismissToast();
     }
 
     @Override
@@ -31,6 +51,15 @@ public class HomeActivity extends Activity implements View.OnClickListener {
                 break;
             case R.id.btn_add:
                 startActivity(new Intent(this, AddClassActivity.class));
+                break;
+            case R.id.btn_homework:
+                startActivity(new Intent(this, HomeworkActivity.class));
+                break;
+            case R.id.btn_myclasses:
+                startActivity(new Intent(this, MyClassesActivity.class));
+                break;
+            default:
+                msgbox.toast(R.string.not_implemented);
                 break;
         }
     }
---
+++ project/src/co/onemeter/oneapp/ui/MyClassesActivity.java
@@ -0,0 +1,32 @@
+package co.onemeter.oneapp.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import co.onemeter.oneapp.R;
+import com.androidquery.AQuery;
+
+/**
+ * 我的课堂页面。
+ * Created by pzy on 11/10/14.
+ */
+public class MyClassesActivity extends Activity implements View.OnClickListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myclasses);
+
+        AQuery q = new AQuery(this);
+
+        q.find(R.id.title_back).clicked(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.title_back:
+                onBackPressed();
+                break;
+        }
+    }
+}

0eae407993cd2372cbfd0d19b7528403f540851a
Zhiyong Pan <zy.pan@wowtech-inc.com>
2014-12-02 19:42:28 +0800

    Revert "更新 dev 版本的 wowtalksdk jar."
    This reverts commit 0e49c09ec76e23a0826a2d82630cdabc9d9e3aa8.

--- dev_tools/http.sh
+++ dev_tools/http.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#curl http://websrv.onemeter.co/om_im_api.php -d "$1" 2>/dev/null
-curl http://dev01-websrv.onemeter.co/om_im_api.php -d "$1" 2>/dev/null
+curl http://websrv.onemeter.co/om_im_api.php -d "$1" 2>/dev/null

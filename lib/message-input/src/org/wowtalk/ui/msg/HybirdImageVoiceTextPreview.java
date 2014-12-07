2b07adfd0e3116c8b04bd4c40d4b7fc45c7f2b1b
Zhiyong Pan <zy.pan@wowtech-inc.com>
2014-12-07 23:01:10 +0800

    添加图文音混合消息播放器（只是一个stub）

---
+++ lib/message-input/src/org/wowtalk/ui/msg/HybirdImageVoiceTextPreview.java
@@ -0,0 +1,16 @@
+package org.wowtalk.ui.msg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * <p>“图文音”混合消息的播放器。</p>
+ * Created by pzy on 12/7/14.
+ */
+public class HybirdImageVoiceTextPreview extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hybird_image_voice_text_player);
+    }
+}

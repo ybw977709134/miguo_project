50f5c6a0e9ae6ce9dcc0ad004e889fdbe8b70014
Pan Zhiyong <panzhiyong84@gmail.com>
2014-12-10 10:18:35 +0800

    add dev_tools/archive_apk.sh

---
+++ dev_tools/archive_apk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Archive APK and proguard mapping files.
+
+build=$1
+echo cp project/oneapp.apk out/om_im_android_$build.apk
+echo cp project/proguard_logs/mapping.txt out/om_im_android_${build}_mapping.txt
+cp project/oneapp.apk out/om_im_android_$build.apk
+cp project/proguard_logs/mapping.txt out/om_im_android_${build}_mapping.txt

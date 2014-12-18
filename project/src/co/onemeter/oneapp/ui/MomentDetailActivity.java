3026f747a3c410f4ff594c96ce786482ce8be281
hutianfeng <hutianfeng@onemeter.co>
2014-12-18 11:38:55 +0800

    更改好友圈内详情页的布局和添加详情页中赞和评论的统计数

--- project/src/co/onemeter/oneapp/ui/MomentDetailActivity.java
+++ project/src/co/onemeter/oneapp/ui/MomentDetailActivity.java
@@ -70,6 +70,11 @@ public class MomentDetailActivity extends Activity implements View.OnClickListen
     private LinearLayout layoutComment;
     private LinearLayout layoutAnswer;
     private ImageView ivMomentFavorite;
+    
+  //详情页中的赞数和评论数
+    private LinearLayout layout_detailcomment_review;
+    private TextView textView_commentdetail_like;//赞
+    private TextView textView_commentdetail_comment;//评论
 
     private WowTalkWebServerIF mWeb;
     private PrefUtil mPrefUtil;
@@ -134,6 +139,12 @@ public class MomentDetailActivity extends Activity implements View.OnClickListen
         layoutComment = (LinearLayout) findViewById(R.id.layout_comment);
         layoutAnswer = (LinearLayout) findViewById(R.id.layout_answer);
         ivMomentFavorite=(ImageView) findViewById(R.id.moment_favorite);
+        
+        //赞和评论的布局
+        layout_detailcomment_review = (LinearLayout) findViewById(R.id.layout_detailcomment_review);
+        layout_detailcomment_review.setVisibility(View.VISIBLE);//显示布局
+        textView_commentdetail_like = (TextView) findViewById(R.id.textView_momentdetail_like);//赞
+        textView_commentdetail_comment = (TextView) findViewById(R.id.textView_momentdetail_comment);//评论
 
         findViewById(R.id.moment_op_btns_layout_whole).setVisibility(View.GONE);
 
@@ -779,7 +790,11 @@ public class MomentDetailActivity extends Activity implements View.OnClickListen
         MomentAdapter.setImageLayout(this, mImageResizer, photoFiles, imageTable);
         int nlikers = MomentAdapter.setViewForLikeReview(this, txtLikeName, moment.reviews);
         int nReviews = MomentAdapter.setViewForCommentReview(this, commentLayout, moment.reviews, -1, moment, this);
-
+        
+      //赞和评论的具体数量统计
+        textView_commentdetail_like.setText(nlikers+"");
+        textView_commentdetail_comment.setText(nReviews+"");
+        
         txtLikeName.setVisibility(nlikers > 0 ? View.VISIBLE : View.GONE);
         reviewLayout.setVisibility(nlikers + nReviews > 0 ? View.VISIBLE : View.GONE);
         reviewDivider.setVisibility(nlikers > 0 && nReviews > 0 ? View.VISIBLE : View.GONE);

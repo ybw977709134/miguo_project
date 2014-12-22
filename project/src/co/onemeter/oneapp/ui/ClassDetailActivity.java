5e77aee3afa6f7af86e74082bbc80293f754e9d0
yuanlei <yuanlei@onemeter.co>
2014-12-22 10:47:29 +0800

    前一个没add lib，现在add了。按照ppt，编写了课堂详情的布局文件

---
+++ project/src/co/onemeter/oneapp/ui/ClassDetailActivity.java
@@ -0,0 +1,16 @@
+package co.onemeter.oneapp.ui;
+
+import co.onemeter.oneapp.R;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ClassDetailActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_class_detail);
+	}
+
+
+}

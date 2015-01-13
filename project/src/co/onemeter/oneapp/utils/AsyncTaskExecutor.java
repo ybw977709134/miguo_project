2efb1e8119c1cb298b232146d996f2b501924c1d
Pan Zhiyong <pan.zhiyong@cybertech.com.cn>
2015-01-13 17:33:39 +0800

    完善 AsyncTaskExecutor

--- project/src/co/onemeter/oneapp/utils/AsyncTaskExecutor.java
+++ project/src/co/onemeter/oneapp/utils/AsyncTaskExecutor.java
@@ -14,20 +14,67 @@ public class AsyncTaskExecutor {
     private static Executor NON_NETWORK_EXECUTOR = Executors.newCachedThreadPool();
     private static Executor SHORT_NETWORK_EXECUTOR = Executors.newCachedThreadPool();
     private static Executor LONG_NETWORK_EXECUTOR = Executors.newCachedThreadPool();
+    private static Executor NEW_TASK_EXECUTOR = new Executor() {
+        @Override
+        public void execute(Runnable runnable) {
+            new Thread(runnable).start();
+        }
+    };
 
-    public static <Params> void executeNonNetworkTask(AsyncTask task, Params... params) {
+    /**
+     * 执行非网络操作。
+     * @param task
+     * @param params
+     * @param <Params>
+     * @param <Progress>
+     * @param <Result>
+     */
+    public static <Params, Progress, Result> void executeNonNetworkTask(
+            AsyncTask<Params, Progress, Result> task, Params... params) {
         executeOnExecutor(task, NON_NETWORK_EXECUTOR, params);
     }
 
-    public static <Params> void executeShortNetworkTask(AsyncTask task, Params... params) {
+    /**
+     * 执行短暂的网络操作，比如一个普通的HTTP请求。
+     * @param task
+     * @param params
+     * @param <Params>
+     * @param <Progress>
+     * @param <Result>
+     */
+    public static <Params, Progress, Result> void executeShortNetworkTask(
+            AsyncTask<Params, Progress, Result> task, Params... params) {
         executeOnExecutor(task, SHORT_NETWORK_EXECUTOR, params);
     }
 
-    public static <Params> void executeLongNetworkTask(AsyncTask task, Params... params) {
+    /**
+     * 执行耗时较长的网络操作，比如上传、下载文件。
+     * @param task
+     * @param params
+     * @param <Params>
+     * @param <Progress>
+     * @param <Result>
+     */
+    public static <Params, Progress, Result> void executeLongNetworkTask(
+            AsyncTask<Params, Progress, Result> task, Params... params) {
         executeOnExecutor(task, LONG_NETWORK_EXECUTOR, params);
     }
 
-    public static <Params> void executeOnExecutor(AsyncTask task, Executor executor, Params... params) {
+    /**
+     * 总是新开一个线程执行任务。
+     * @param task
+     * @param params
+     * @param <Params>
+     * @param <Progress>
+     * @param <Result>
+     */
+    public static <Params, Progress, Result> void executeNew(
+            AsyncTask<Params, Progress, Result> task, Params... params) {
+        executeOnExecutor(task, NEW_TASK_EXECUTOR, params);
+    }
+
+    public static <Params, Progress, Result> void executeOnExecutor(
+            AsyncTask<Params, Progress, Result> task, Executor executor, Params... params) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             task.executeOnExecutor(executor, params);
         } else {

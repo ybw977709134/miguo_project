27f486f4dc0a62c876ce0342168cf75bbdd7380b
Pan Zhiyong <zy.pan@wowtech-inc.com>
2015-02-03 22:43:25 +0800

    dev_tools/http.sh

--- dev_tools/http.sh
+++ dev_tools/http.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+curl http://websrv.onemeter.co/om_im_api.php -d "$1" 2>/dev/null
 curl http://dev01-websrv.onemeter.co/om_im_api.php -d "$1" 2>/dev/null
-#curl http://localhost/om_im_api.php -d "$1" 2>/dev/null
+curl http://localhost/om_im_api.php -d "$1" 2>/dev/null

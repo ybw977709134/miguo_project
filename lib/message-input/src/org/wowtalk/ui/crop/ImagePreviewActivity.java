6e069c3a34bc8fcbe7303f108f6f7e62f6a9481b
yuanlei <yuanlei@onemeter.co>
2015-02-04 18:27:16 +0800

    进一步完成消息通讯图片选择截图功能，那部分没有完全完成所以不要测试

--- lib/message-input/src/org/wowtalk/ui/crop/ImagePreviewActivity.java
+++ lib/message-input/src/org/wowtalk/ui/crop/ImagePreviewActivity.java
@@ -1,5 +1,9 @@
 package org.wowtalk.ui.crop;
 
+/**
+ * creaeted by jacky on 2/4/2015
+ * to show photo picked ,need uri
+ */
 import java.io.File;
 
 import org.wowtalk.ui.msg.R;
@@ -8,23 +12,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class ImagePreviewActivity extends Activity {
+public class ImagePreviewActivity extends Activity implements OnClickListener {
 	private ImageView resultView;
 	public static final String INPUTURI = "uri";
-
+	private Uri imagePickedUri = null;
+	private Uri resultUri = null;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_preview);
         resultView = (ImageView) findViewById(R.id.result_image);
         Intent intent = getIntent();
-        Uri imageUri = intent.getData();
-        if(imageUri != null){
-        	resultView.setImageURI(imageUri);
+        imagePickedUri = intent.getData();
+        if(imagePickedUri != null){
+        	resultView.setImageURI(imagePickedUri);
         }
+        findViewById(R.id.btn_crop).setOnClickListener(this);
+        findViewById(R.id.btn_send).setOnClickListener(this);
+        findViewById(R.id.btn_cancel).setOnClickListener(this);
     }
 
     @Override
@@ -43,9 +54,31 @@ public class ImagePreviewActivity extends Activity {
 
     private void handleCrop(int resultCode, Intent result) {
         if (resultCode == RESULT_OK) {
+        	resultUri = Crop.getOutput(result);
             resultView.setImageURI(Crop.getOutput(result));
         } else if (resultCode == Crop.RESULT_ERROR) {
             Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
+
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+		if(id == R.id.btn_send){
+			if(imagePickedUri != null){
+				if(resultUri != null){
+					setResult(RESULT_OK, new Intent().setData(resultUri));
+				}else{
+					setResult(RESULT_OK, new Intent().setData(imagePickedUri));
+				}
+				finish();
+			}
+		}else if(id == R.id.btn_cancel){
+			finish();
+		}else if(id == R.id.btn_crop){
+			if(imagePickedUri != null){
+					beginCrop(imagePickedUri);
+			}
+		}
+	}
 }

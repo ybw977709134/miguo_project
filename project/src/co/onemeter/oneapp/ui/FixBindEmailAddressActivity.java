3b0a8abf14b52991fe87746075f800dc177a55d5
hutianfeng <hutianfeng@onemeter.co>
2015-03-03 18:44:26 +0800

    完成绑定邮箱和部分修改邮箱的UI

---
+++ project/src/co/onemeter/oneapp/ui/FixBindEmailAddressActivity.java
@@ -0,0 +1,13 @@
+package co.onemeter.oneapp.ui;
+
+import co.onemeter.oneapp.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FixBindEmailAddressActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bind_email_adress);
+	}
+}

e2f4af24088f7a1bf79c52e6ecb7edced5461162
Pan Zhiyong <zy.pan@wowtech-inc.com>
2015-03-19 23:25:26 +0800

    update dev_tools/archive_apk.sh

--- dev_tools/archive_apk.sh
+++ dev_tools/archive_apk.sh
@@ -2,7 +2,7 @@
 # Archive APK and proguard mapping files.
 
 build=$1
-echo cp project/oneapp.apk out/om_im_android_$build.apk
+echo cp project/project.apk out/om_im_android_$build.apk
 echo cp project/proguard_logs/mapping.txt out/om_im_android_${build}_mapping.txt
-cp project/oneapp.apk out/om_im_android_$build.apk
+cp project/project.apk out/om_im_android_$build.apk
 cp project/proguard_logs/mapping.txt out/om_im_android_${build}_mapping.txt

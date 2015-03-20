703576091f7c58c8fe20e667554d5616a55fed98
Pan Zhiyong <zy.pan@wowtech-inc.com>
2015-03-20 20:35:46 +0800

    add bash script to build & sign

---
+++ dev_tools/build_with_gradle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+gradle clean
+gradle assembleRelease
+
+src=./project/build/outputs/apk/project-release-unsigned.apk
+dest=./project/project-release.apk
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
+    -keystore doc/release.keystore -storepass qwerty \
+    $src onemeter
+
+jarsigner -verify -verbose -certs $src
+
+if [ -f $dest ];
+then
+    rm -f $dest
+fi
+$ANDROID_HOME/build-tools/21.1.2/zipalign -v 4 $src $dest
+
+echo output to $dest

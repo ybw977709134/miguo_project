0181afe65005cbe352df0ff594920b927186f57e
Pan Zhiyong <zy.pan@wowtech-inc.com>
2015-03-21 17:22:52 +0800

    在 gradle 中通过环境变量引用不同的 om_im_sdk，从而可以用同一份代码库分别打包测试版和产品版。

--- dev_tools/build_with_gradle.sh
+++ dev_tools/build_with_gradle.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-gradle clean
-gradle assembleRelease
+omenv='dev01'
+[ $# == 1 ] && omenv=$1
+
+export OM_ENV=$omenv
+
+echo OM_ENV: $OM_ENV 
 
 src=./project/build/outputs/apk/project-release.apk
-dest=./project/project.apk
+dest=./project/project-$omenv.apk
 
-cp $src $dest
-echo output to $dest
+gradle clean \
+    && gradle assembleRelease \
+    && cp $src $dest \
+    && echo output to $dest

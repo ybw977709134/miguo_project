a38462b7040c476ba5b0b0718363dd6d450c0c8a
yuanlei <yuanlei@onemeter.co>
2015-03-30 16:11:05 +0800

    完成首页拍照答疑功能ui还需优化，在线作业拍照有提示框

--- project/src/co/onemeter/oneapp/ui/STSmsFragment.java
+++ project/src/co/onemeter/oneapp/ui/STSmsFragment.java
@@ -1,30 +1,68 @@
 package co.onemeter.oneapp.ui;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import org.wowtalk.api.ChatMessage;
+import org.wowtalk.api.Database;
+import org.wowtalk.api.GroupChatRoom;
+
+import java.util.ArrayList;
+
 import co.onemeter.oneapp.R;
+import co.onemeter.oneapp.adapter.MessagesAdapter;
 
 /**
  * Created by jacky on 15-3-27.
  */
-public class STSmsFragment extends Fragment{
+public class STSmsFragment extends Fragment implements AdapterView.OnItemClickListener {
 
-    private ListView listview_sms;
+    private String[] path;
+
+    private ArrayList<ChatMessage> messages = new ArrayList<>();
+    private MessagesAdapter adapter;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        path = getArguments().getStringArray(SendToActivity.INTENT_PAHT);
+        loadMsgsFromLocal();
+        adapter = new MessagesAdapter(getActivity(),messages);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View contentView = inflater.inflate(R.layout.sms_page,container,false);
         contentView.findViewById(R.id.title_bar).setVisibility(View.GONE);
         contentView.findViewById(R.id.layout_bg).setVisibility(View.GONE);
         contentView.findViewById(R.id.search_glass_img).setVisibility(View.GONE);
 
-        listview_sms = (ListView) contentView.findViewById(R.id.sms_list);
+        ListView listview_sms = (ListView) contentView.findViewById(R.id.sms_list);
+        listview_sms.setAdapter(adapter);
+        listview_sms.setOnItemClickListener(this);
         return contentView;
+    }
+
+    private void loadMsgsFromLocal(){
+        int initCount = 20;
+        Database dbHelper = new Database(getActivity());
+        messages.addAll(dbHelper.getLatestedMessages(initCount));
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+       ChatMessage msg = messages.get(position);
+        Context context = getActivity();
+        if(msg.isGroupChatMessage()){
+            MessageComposerActivity.launchToChatWithGroupWithPicture(context,msg.chatUserName,path,true);
+        }else{
+            MessageComposerActivity.launchToChatWithBuddyWithPicture(context,msg.chatUserName,path,true);
+        }
     }
 }

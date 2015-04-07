c0d3f9fbd7864332748e2577a7350e189cb1d234
Pan Zhiyong <zy.pan@wowtech-inc.com>
2015-04-07 23:00:24 +0800

    output date

--- dev_tools/gather.sh
+++ dev_tools/gather.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # 收集各开发分支上的新内容
 
+log_opt='--pretty="%h |%ar|%an|%s"'
+
 # determine check point
-chkpnt_dev_as=`git log --oneline -n1 dev_as | col.sh 1`
-chkpnt_dev=`git log --oneline -n1 dev | col.sh 1`
+chkpnt_dev_as=`sh -c "git log $log_opt -n1 dev_as" | col.sh 1`
+chkpnt_dev=`sh -c "git log $log_opt -n1 dev" | col.sh 1`
+
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo check point of branch dev_as: $chkpnt_dev_as
 echo check point of branch dev: $chkpnt_dev
@@ -12,23 +15,23 @@ echo
 dst_branch=dev_as
 src_branch=origin/dev_yuanlei
 echo $src_branch
-cmd="git log --oneline --no-merges $chkpnt_dev_as..$dst_branch $src_branch"
-echo git cherry-pick `$cmd | col.sh 1 | tac`
-$cmd
+cmd="git log $log_opt --no-merges $chkpnt_dev_as..$dst_branch $src_branch"
+echo git cherry-pick `sh -c "$cmd" | col.sh 1 | tac`
+sh -c "$cmd"
 echo
 
 dst_branch=dev
 src_branch=origin/dev_hutianfeng
 echo $src_branch
-cmd="git log --oneline --no-merges $chkpnt_dev..$dst_branch $src_branch"
-echo git cherry-pick `$cmd | col.sh 1 | tac`
-$cmd
+cmd="git log $log_opt --no-merges $chkpnt_dev..$dst_branch $src_branch"
+echo git cherry-pick `sh -c "$cmd" | col.sh 1 | tac`
+sh -c "$cmd"
 echo
 
 dst_branch=dev
 src_branch=origin/dev_zhangzheng
 echo $src_branch
-cmd="git log --oneline --no-merges $chkpnt_dev..$dst_branch $src_branch"
-echo git cherry-pick `$cmd | col.sh 1 | tac`
-$cmd
+cmd="git log $log_opt --no-merges $chkpnt_dev..$dst_branch $src_branch"
+echo git cherry-pick `sh -c "$cmd" | col.sh 1 | tac`
+sh -c "$cmd"
 echo

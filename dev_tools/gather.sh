1120e8a0f9e1da49f5968c8faad9a14e5228c12b
Pan Zhiyong <zy.pan@wowtech-inc.com>
2015-05-14 19:58:15 +0800

    update dev_tools/gather.sh

--- dev_tools/gather.sh
+++ dev_tools/gather.sh
@@ -2,49 +2,25 @@
 # 收集各开发分支上的新内容
 # 
 # 执行之前请：
-# git checkout dev_as
-# git pull
+# git fetch
 
-log_opt='--pretty="%h |%ar|%an|%s"'
+function gitlog {
+    author=$1
+    src_branch=$2
 
-# determine check point
-chkpnt_dev_as=`sh -c "git log $log_opt -n1 dev_as" | col.sh 1`
-chkpnt_dev=`sh -c "git log $log_opt -n1 dev" | col.sh 1`
+    echo by $author:
 
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo check point of branch dev_as: $chkpnt_dev_as
-echo check point of branch dev: $chkpnt_dev
-echo
+    range="--since=`date -d '1 days ago' +%Y-%m-%d`"
+    log_opt='--pretty="%h | %ar | %s"'
 
-src_branch=dev_as
-range="--since=`date -d '1 days ago' +%Y-%m-%d`"
-echo $src_branch
-cmd="git log $log_opt --no-merges $range $src_branch"
-pick_dev_as="git cherry-pick `sh -c "$cmd" | col.sh 1 | tac`"
-echo $pick_dev_as
-sh -c "$cmd"
-echo
+    cmd="git log $log_opt --author $author --no-merges $range $src_branch"
+    pick_dev_as="git cherry-pick `sh -c "$cmd" | col.sh 1 | tac`"
+    sh -c "$cmd"
+    echo '=>' $pick_dev_as
+    echo '=>' git merge $src_branch
+    echo
+}
 
-dst_branch=dev
-src_branch=origin/dev_zhangzheng
-echo $src_branch
-cmd="git log $log_opt --no-merges $chkpnt_dev..$dst_branch $src_branch"
-pick_zhang="git cherry-pick `sh -c "$cmd" | col.sh 1 | tac`"
-echo $pick_zhang
-sh -c "$cmd"
-echo
+gitlog hutianfeng origin/dev_as
+gitlog zhangzheng origin/dev_zhangzheng
 
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo \# for dev_as
-echo git checkout dev_as \\
-echo \&\& $pick_zhang 
-
-echo ''
-echo \# for dev
-echo git checkout dev \\
-echo \&\& git merge origin/dev_zhangzheng \\
-echo \&\& $pick_dev_as
-
-echo ''
-echo \# check
-echo git diff dev_as dev project/src
